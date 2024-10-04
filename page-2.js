08898a48cdd69a15b8e0767548bb829d9dcb9888
juliasobocinska <118121699+juliasobocinska@users.noreply.github.com>
2024-10-04 14:39:04 +0000

    Deploying to gh-pages from @ juliasobocinska/goit-js-hw-.11@4afe602b64047ae6bc96a118b7f80639167cd20f 🚀

--- page-2.js
+++ page-2.js
@@ -1,12 +1,14 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as h,S as p}from"./assets/vendor-5ObWk2rO.js";/* empty css                      */const n=document.querySelector(".loader"),s=document.querySelector(".gallery");let i;document.getElementById("searchButton").addEventListener("click",function(r){r.preventDefault();const l=document.getElementById("searchInput").value;n.style.display="block";const o=new URLSearchParams({key:"46327041-9a6335f12388e2a1236167102",q:l,image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(o.toString());const a=`https://pixabay.com/api/?${o}`;console.log(a),fetch(a).then(e=>{if(!e.ok)throw new Error(e.error);return e.json()}).then(e=>{if(s.innerHTML="",Array.isArray(e.hits)&&e.hits.length===0)h.error({title:!1,message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});else{const c=e.hits.map(t=>`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i as p,S as h}from"./assets/vendor-5ObWk2rO.js";/* empty css                      */const n=document.querySelector(".loader"),r=document.querySelector(".gallery");let c;document.getElementById("searchButton").addEventListener("click",function(t){t.preventDefault();const l=document.getElementById("searchInput").value;n.style.display="block";const a=new URLSearchParams({key:"46327041-9a6335f12388e2a1236167102",q:l,image_type:"photo",orientation:"horizontal",safesearch:!0});console.log(a.toString());const o=`https://pixabay.com/api/?${a}`;console.log(o),fetch(o).then(e=>{if(!e.ok)throw new Error(e.error);return e.json()}).then(e=>{if(r.innerHTML="",Array.isArray(e.hits)&&e.hits.length===0)p.error({title:!1,message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"});else{const i=e.hits.map(s=>`
                     <div class= "card">
-                    <a href="${t.largeImageURL}" class="lightbox" title="${t.tags}">
-                    <img src="${t.webformatURL}" alt="${t.tags}"></img>
+                    <a href="${s.largeImageURL}" class="lightbox" title="${s.tags}">
+                    <img src="${s.webformatURL}" alt="${s.tags}"></img>
                     </a>
-                    <p>Likes: ${t.likes}</p>
-                    <p>Views: ${t.views}</p>
-                    <p>Comments: ${t.comments}</p>
-                    <p>Downloads: ${t.downloads}</p>
+                    <div class="container-box">
+                    <p class="desc"><span class="count">Likes:</span> ${s.likes}</p>
+                    <p class="desc"><span class="count">Views:</span> ${s.views}</p>
+                    <p class="desc"><span class="count">Comments:</span> ${s.comments}</p>
+                    <p class="desc"><span class="count">Downloads:</span> ${s.downloads}</p>
                     </div>
-                    `).join("");s.innerHTML=c,i=new p(".gallery a",{captionsData:"alt",sourceAttr:"href",captions:!0,captionDelay:250}),i.refresh()}}).catch(e=>{console.error("There has been a problem with your fetch operation:",e)}).finally(()=>{n.style.display="none"})});document.addEventListener("keydown",r=>{r.key==="Escape"&&instance.visible()&&instance.close()});
+                    </div>
+                    `).join("");r.innerHTML=i,c=new h(".gallery a",{captionsData:"alt",sourceAttr:"href",captions:!0,captionDelay:250}),c.refresh()}}).catch(e=>{console.log("There has been a problem with your fetch operation:",e)}).finally(()=>{n.style.display="none"})});document.addEventListener("keydown",t=>{t.key==="Escape"&&instance.visible()&&instance.close()});
 //# sourceMappingURL=page-2.js.map
